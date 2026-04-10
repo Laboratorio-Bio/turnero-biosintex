524e2531964225534ec25f952915e184d13c9fc1
pabloserodioofar-create <pabloserodio.ofar@gmail.com>
2026-04-10 08:35:49 -0300

    Add files via upload

---
+++ config.js
@@ -0,0 +1,6 @@
+
+const CONFIG = {
+    API_URL: "https://script.google.com/macros/s/AKfycbyrQjtHW7I01-r4TNJvMhqJb7py8NZucktI-mfe40eB_Tws-quJY1NfW9R6FevX9Lxyxw/exec",
+    PLANTAS: ["Barracas", "Pibera"],
+    FERIADOS_2026: ['2026-01-01', '2026-02-16', '2026-02-17', '2026-03-24', '2026-04-02', '2026-04-03', '2026-05-01', '2026-05-25', '2026-06-20', '2026-07-09', '2026-12-08', '2026-12-25']
+};
